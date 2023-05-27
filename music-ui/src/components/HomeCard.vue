1fad5a06cf07a59d242e91a0230d8747d9ead66a
Nicola Beleznay <Nicola.Beleznay+git@gmail.com>
2023-05-27 15:52:16 -0700

    basic home page finished

---
+++ music-ui/src/components/HomeCard.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { toRefs, ref, onMounted } from 'vue';
+import { callAPI } from '../api/dbapi'
+
+const props = defineProps({
+    linkto: String,
+    linkid: Number
+})
+const { linkto, linkid } = toRefs(props)
+
+const url = linkto.value+'/'+linkid.value
+const link = ref(null)
+onMounted(async () => {
+    link.value = await callAPI(url)
+})
+</script>
+
+<template>
+    <p>{{ (link === null) ? '' : link.name }}</p>
+</template>
--- music-ui/src/views/HomeView.vue
+++ music-ui/src/views/HomeView.vue
@@ -1,6 +1,29 @@
 <script setup>
+import HomeCard from '../components/HomeCard.vue';
+
+// get random integer from 1 to n
+function getRandom(n) {
+  return Math.floor(Math.random() * n)+1
+}
 </script>
 
 <template>
-  <div>Home View</div>
+  <h3>Featured Artists:</h3>
+  <div>
+    <HomeCard linkto="artists" :linkid=getRandom(6) />
+    <HomeCard linkto="artists" :linkid=getRandom(6) />
+    <HomeCard linkto="artists" :linkid=getRandom(6) />
+  </div>
+  <h3>Featured Albums:</h3>
+  <div>
+    <HomeCard linkto="albums" :linkid=getRandom(10) />
+    <HomeCard linkto="albums" :linkid=getRandom(10) />
+    <HomeCard linkto="albums" :linkid=getRandom(10) />
+  </div>
+  <h3>Featured Songs:</h3>
+  <div>
+    <HomeCard linkto="songs" :linkid=getRandom(10) />
+    <HomeCard linkto="songs" :linkid=getRandom(10) />
+    <HomeCard linkto="songs" :linkid=getRandom(10) />
+  </div>
 </template>
