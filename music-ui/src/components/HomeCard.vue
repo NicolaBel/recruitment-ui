aa5efa59a307c79917cf94508481de288919f060
Nicola Beleznay <Nicola.Beleznay+git@gmail.com>
2023-05-28 12:09:09 -0700

    added link to router page

--- music-ui/src/components/HomeCard.vue
+++ music-ui/src/components/HomeCard.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { toRefs, ref, onMounted } from 'vue';
 import { callAPI } from '../api/dbapi'
+import { RouterLink } from 'vue-router';
 
 const props = defineProps({
     linkto: String,
@@ -16,5 +17,7 @@ onMounted(async () => {
 </script>
 
 <template>
+  <RouterLink :to="linkto">
     <p>{{ (link === null) ? '' : link.name }}</p>
+  </RouterLink>
 </template>
