1cbf25d9263c36791399e23a9540b619d3641d80
Nicola Beleznay <Nicola.Beleznay+git@gmail.com>
2023-05-28 22:08:58 -0700

    converted AlbumsView to use ApiView

--- music-ui/src/views/AlbumsView.vue
+++ music-ui/src/views/AlbumsView.vue
@@ -1,81 +1,43 @@
 <script setup>
-import { ref, onMounted, computed } from 'vue'
-import { apiGet } from '../api/dbapi'
+import { ref } from 'vue'
 import BaseView from '../components/BaseView.vue';
-import Paging from '../components/Paging.vue';
+import ApiView from '../components/ApiView.vue';
 
-// declarations
-const albums = ref(null)
-const baseUrl = 'albums?_limit=10&_expand=artist'
-let sortUrl = ''
-let sortBy = 'asc'
-let page = ref(1)
-let pageUrl = ref(computed({
-    get() {
-        return '&_page='+page.value
-    }
-    //no setter, readonly
-}))
 let showName= ref(false)
 
-// functions
-async function pagePrev() {
-    page.value--
-    albums.value = await apiGet(baseUrl+pageUrl.value+sortUrl)
-}
-async function pageNext() {
-    page.value++
-    albums.value = await apiGet(baseUrl+pageUrl.value+sortUrl)
-}
-
-async function doSort(col) {
+async function doSortReset() {
     showName.value = false//reset, cannot sorting by this column, at this time
-    sortUrl = '&_sort='+col
-    if (sortBy == 'asc') {
-        sortUrl = sortUrl+'&_order=asc'
-        sortBy = 'desc'
-    } else if (sortBy == 'desc') {
-        sortUrl = sortUrl+'&_order=desc'
-        sortBy = 'asc'
-    } else {
-        sortUrl = ''
-        sortBy = 'asc'
-    }
-    albums.value = await apiGet(baseUrl+pageUrl.value+sortUrl)
 }
-
-// setup
-onMounted(async () => {
-    albums.value = await apiGet(baseUrl+pageUrl.value+sortUrl)
-})
+const titles = [
+    {
+        sortBy: 'artist_id',
+        col: 'Artist Id'
+    },
+    {
+        sortBy: 'name',
+        col: 'Artist Name'
+    },
+    {
+        sortBy: 'year_released',
+        col: 'Year Released'
+    }
+]
 </script>
 
 <template>
   <BaseView>
     <template #heading>Albums View</template>
-    <Paging :cur-page="page" @prev-Page="pagePrev" @next-Page="pageNext"/>
-    <div>
-      <input type="checkbox" id="showNameArtist" value={{showName}} v-model="showName">
-      <label for="showNameArtist">Show Artist name</label>
-    </div>
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th @click="doSort(`artist_id`)">Artist Id</th>
-            <th @click="doSort(`name`)">Name</th>
-            <th @click="doSort(`year_released`)">Year Released</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="album in albums">
-            <td v-if="showName">{{ album.artist.name }}</td>
-            <td v-else>{{ album.artist_id }}</td>
-            <td>{{ album.name }}</td>
-            <td>{{ album.year_released }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <ApiView db-table="albums" db-table-parent="artist" :db-table-headings="titles" @do-sort-reset="doSortReset">
+        <template #extras-top>
+            <input type="checkbox" id="showNameArtist" value={{showName}} v-model="showName">
+            <label for="showNameArtist">Show Artist name</label>
+        </template>
+        <template #table-details="{id, name, artist_id, year_released, artist}">
+            <td v-if="showName">{{ artist.name }}</td>
+            <td v-else>{{ artist_id }}</td>
+            <td>{{ name }}</td>
+            <td>{{ year_released }}</td>
+        </template>
+    </ApiView>
   </BaseView>
 </template>
