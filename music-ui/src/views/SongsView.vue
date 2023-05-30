f79e9944d42d3408c2d4565db19a8fad746edd4f
Nicola Beleznay <Nicola.Beleznay+git@gmail.com>
2023-05-29 20:24:11 -0700

    added show album name checkbox

--- music-ui/src/views/SongsView.vue
+++ music-ui/src/views/SongsView.vue
@@ -1,7 +1,13 @@
 <script setup>
+import { ref } from 'vue'
 import BaseView from '../components/BaseView.vue';
 import ApiView from '../components/ApiView.vue';
 
+let showName = ref(false)
+
+async function doSortReset() {
+    showName.value = false//reset, cannot sort  by this column, at this time
+}
 const titles = [
     {
         sortBy: 'album_id',
@@ -21,9 +27,14 @@ const titles = [
 <template>
   <BaseView>
     <template #heading>Songs View</template>
-    <ApiView db-table="songs" :db-table-headings="titles">
-      <template #table-details="{album_id, track, id, name}">
-        <td>{{ album_id }}</td>
+    <ApiView db-table="songs" db-table-parent="album" :db-table-headings="titles" @do-sort-reset="doSortReset">
+      <template #extras-top>
+        <input type="checkbox"  id="showNameAlbum" value={{showName}} v-model="showName">
+        <label for="showNameAlbum">Show Album Name</label>
+      </template>
+      <template #table-details="{album_id, track, id, name, album}">
+        <td v-if="showName">{{ album.name }}</td>
+        <td v-else>{{ album_id }}</td>
         <td>{{ track }}</td>
         <td>{{ name }}</td>
       </template>  
